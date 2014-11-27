f708529c8d078ef3da8d9ad546848347632eca07
Chase Bradbury <chasebradbury@gmail.com>
2014-11-26 18:04:50 -0800

    Initial Build
    Initial build for the swiping app

---
+++ SwiperTest/ViewController.h
@@ -0,0 +1,45 @@
+//
+//  ViewController.h
+//  SwiperTest
+//
+//  Created by Chase Bradbury on 7/23/14.
+//  Copyright (c) 2014 Redwood Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef enum {
+    up,
+    down,
+    left,
+    right
+} RSDirection;
+
+@interface ViewController : UIViewController
+{
+    IBOutlet UIButton *startButton;
+    IBOutlet UIImageView *arrowImage;
+    IBOutlet UISwipeGestureRecognizer *swipeControl;
+    IBOutlet UILabel *timeLabel;
+    IBOutlet UILabel *scoreLabel;
+    IBOutlet UILabel *highscoreLabel;
+    IBOutlet UILabel *genericLabel;
+    
+    IBOutlet UILabel *fudge;
+    
+    RSDirection direction;
+    NSInteger score;
+    NSInteger highscore;
+    NSInteger repMax;
+    NSInteger timeMax;
+    NSInteger time;
+    NSTimer *timer;
+    
+    float interval;
+}
+
+- (IBAction)startGame;
+
+- (IBAction)swipeAction;
+
+@end
---
+++ SwiperTest/ViewController.m
@@ -0,0 +1,184 @@
+//
+//  ViewController.m
+//  SwiperTest
+//
+//  Created by Chase Bradbury on 7/23/14.
+//  Copyright (c) 2014 Redwood Studios. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    [self restartGame];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)restartGame
+{
+    direction = up;
+    score = -1;
+    timeMax = 3;
+    time = timeMax;
+    interval = 1.5f;
+    
+    repMax = 3;
+    
+    // Change this
+    //highscore = 0;
+    
+    startButton.hidden = NO;
+    scoreLabel.hidden = YES;
+    genericLabel.hidden = YES;
+}
+
+- (IBAction)startGame
+{
+    startButton.hidden = YES;
+    swipeControl.enabled = YES;
+    arrowImage.hidden = NO;
+    timeLabel.hidden = NO;
+    scoreLabel.hidden = NO;
+    highscoreLabel.hidden = YES;
+    
+    [self swipeAction];
+    
+    timeLabel.text = [NSString stringWithFormat:@"%i", time];
+    
+    [timer invalidate];
+    timer = nil;
+    
+    [self setTimerFor:interval Repeats:repMax];
+}
+
+- (void)endGame
+{
+    swipeControl.enabled = NO;
+    arrowImage.hidden = YES;
+    timeLabel.hidden = YES;
+    highscoreLabel.hidden = NO;
+    genericLabel.hidden = NO;
+    
+    genericLabel.text = @"Game over!";
+    
+    [timer invalidate];
+    timer = nil;
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0f
+                                             target:self
+                                           selector:@selector(changeHighscore)
+                                           userInfo:nil
+                                            repeats:NO];
+}
+
+- (void)changeHighscore
+{
+    if (score > highscore)
+    {
+        genericLabel.text = @"New highscore!";
+        highscore = score;
+        highscoreLabel.text = [NSString stringWithFormat:@"Highscore: %i", highscore];
+        
+        [timer invalidate];
+        timer = nil;
+        
+        timer = [NSTimer scheduledTimerWithTimeInterval:1.0f
+                                                 target:self
+                                               selector:@selector(restartGame)
+                                               userInfo:nil
+                                                repeats:NO];
+    }
+    else
+    {
+        [self restartGame];
+    }
+    
+}
+
+- (IBAction)swipeAction
+{
+    if (interval > 1.0)
+    {
+        interval -= 0.02;
+    } else if (interval > 0.5)
+    {
+        interval -= 0.01;
+    } else if (score % 50 == 0 && interval > 0.3)
+    {
+        interval -= 0.01;
+    }
+    
+    time = timeMax;
+    timeLabel.text = [NSString stringWithFormat:@"%i", time];
+    
+    scoreLabel.text = [NSString stringWithFormat:@"Score: %i", ++score];
+    
+    [timer invalidate];
+    timer = nil;
+    
+    [self setTimerFor:interval Repeats:repMax];
+    
+    NSLog(@"Swiped.");
+    
+    // Get a random direction
+    direction = arc4random() % 4;
+    
+    if (direction == up)
+    {
+        NSLog(@"Swipe up!");
+        [swipeControl setDirection:UISwipeGestureRecognizerDirectionUp];
+        arrowImage.image = [UIImage imageNamed:@"arrowUp"];
+    }
+    else if (direction == down)
+    {
+        NSLog(@"Swipe down!");
+        [swipeControl setDirection:UISwipeGestureRecognizerDirectionDown];
+        arrowImage.image = [UIImage imageNamed:@"arrowDown"];
+    }
+    else if (direction == left)
+    {
+        NSLog(@"Swipe left!");
+        [swipeControl setDirection:UISwipeGestureRecognizerDirectionLeft];
+        arrowImage.image = [UIImage imageNamed:@"arrowLeft"];
+    }
+    else
+    {
+        NSLog(@"Swipe right!");
+        [swipeControl setDirection:UISwipeGestureRecognizerDirectionRight];
+        arrowImage.image = [UIImage imageNamed:@"arrowRight"];
+    }
+    
+}
+
+- (void)countDown
+{
+    timeLabel.text = [NSString stringWithFormat:@"%i", --time];
+    
+    if (time < 0) {
+        [self endGame];
+    }
+}
+
+- (void)setTimerFor:(float)seconds Repeats:(NSInteger)reps
+{
+    timer = [NSTimer scheduledTimerWithTimeInterval:seconds/reps
+                                             target:self
+                                           selector:@selector(countDown)
+                                           userInfo:nil
+                                            repeats:YES];
+}
+
+@end
